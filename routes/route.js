1b06205744c0962d0c6f4cae0268c8a294033c52
Chinmay Mokashi <chinmay.mokashi@quantiphi.com>
2019-01-18 14:14:47 +0530

    Completed router.js

--- routes/route.js
+++ routes/route.js
@@ -29,32 +29,31 @@ router.get(
       json: true
     };
     // Send request to authorization API
-    // console.log(send_to_authorization);
     request(send_to_authorization, (error_2, response_2, body_2) => {
       if (error_2) {
         response_1.send({
           ERROR: error_2
         });
       } else {
-        if (!response_2.body["AUTHORIZATION_TOKEN"]) {
+        if (!response_2.body.hasOwnProperty("AUTHORIZATION_TOKEN")) {
           response_1.send(response_2.body);
         } else {
-          // Send request to appropriate API with authorization
-          //   const send_to_api = {
-          //     method: "POST",
-          //     url: Routes.routes[request_1.params.api_name.toUpperCase()],
-          //     headers: {
-          //       "Content-Type": "application/json",
-          //       "x-auth-token": response_2.body["AUTHORIZATION_TOKEN"]
-          //     },
-          //     body: request_1.body,
-          //     json: true
-          //   };
-          //   // Send request to appropriate API
-          //   request(send_to_api, (error_3, response_3, body_3) => {
-          //     response_1.send(response_3.body);
-          //   });
-          response_1.send(response_2.body["AUTHORIZATION_TOKEN"]);
+          //   Send request to appropriate API with authorization
+          const send_to_api = {
+            method: "POST",
+            url: Routes.routes[request_1.params.api_name.toUpperCase()],
+            headers: {
+              "Content-Type": "application/json",
+              "x-auth-token": response_2.body["AUTHORIZATION_TOKEN"]
+            },
+            body: request_1.body,
+            json: true
+          };
+          // Send request to appropriate API
+          request(send_to_api, (error_3, response_3, body_3) => {
+            response_1.send(response_3.body);
+          });
+          response_1.send(response_2.body);
         }
       }
     });
